178299a055860d7335b757f745b3650cf6d7a8fb
kunansy <kolobov.kirill@list.ru>
2021-11-27 17:42:05 +0300

    settings updated
    * timeout default value set
    * URLS_PATH added
    * getting urls, user agents from URLS_PATH added
    * clearing environ after init added

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from environs import Env
 
 env = Env()
@@ -11,10 +14,15 @@ REQUEST_TIMEOUT = env.int("REQUEST_TIMEOUT", 5)
 MAX_DEPTH = env.int("MAX_DEPTH", 25)
 MIN_SLEEP = env.int("MIN_SLEEP", 3)
 MAX_SLEEP = env.int("MAX_SLEEP", 6)
-TIMEOUT = env.int("TIMEOUT")
+TIMEOUT = env.int("TIMEOUT", 0)
+LOG_LEVEL = env.log_level("LOG_LEVEL", 'debug')
+URLS_PATH = env.path("URLS_PATH", "./urls.json")
 
-ROOT_URLS = env.list("ROOT_URLS")
-BLACKLISTED_URLS = env.list("blacklisted_urls")
-USER_AGENTS = env.list("USER_AGENTS")
+with URLS_PATH.open() as f:
+    urls = json.load(f)
 
-LOG_LEVEL = env.log_level("LOG_LEVEL")
+ROOT_URLS = urls.get("ROOT_URLS", [])
+BLACKLISTED_URLS = urls.get("blacklisted_urls", [])
+USER_AGENTS = urls.get("USER_AGENTS", [])
+
+os.environ.clear()
